8fac966180a3abcc111435b1709f06ff27ef2e4b
unknown <trigork@gmail.com>
2022-03-13 19:34:08 +0100

    Restructured package

---
+++ pgtd/__init__.py
@@ -0,0 +1,4 @@
+from .difficulties import Difficulty
+from .exceptions import IncorrectStateError, GameAlreadyStartedError, GameIsOverError, UnfinishedGameError
+from .thrilldigger import PlayState, ThrillDigger
+from .board import Item, CellState
